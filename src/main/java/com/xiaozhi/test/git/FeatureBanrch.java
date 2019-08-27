2b1661dfd06a38878be74ce575f96655628625c6
黄智 <huangzhi@supermonkey.com.cn>
2019-08-27 11:49:55 +0800

    feature二次commit
    feature二次commit

--- src/main/java/com/xiaozhi/test/git/FeatureBanrch.java
+++ src/main/java/com/xiaozhi/test/git/FeatureBanrch.java
@@ -9,5 +9,6 @@ public class FeatureBanrch {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("commit1");
+        System.out.println("commit2");
     }
 }
